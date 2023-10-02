2d79075639bb38164caf41344d4849d5ab558769
tvrbosic <tomislav.vrbosic@gmail.com>
2023-10-02 12:39:29 +0200

    02_10_2023

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -27,8 +27,77 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("Name", "The Display Order field cannot exactly match category name.");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.Find(id);
+            //Category? category = _db.Categories.FirstOrDefault(x => x.Id == id);
+            //Category? category = _db.Categories.Where(x => x.Id == id).FirstOrDefault();
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? obj = _db.Categories.Find(id);
+            //Category? obj= _db.Categories.FirstOrDefault(x => x.Id == id);
+            //Category? obj= _db.Categories.Where(x => x.Id == id).FirstOrDefault();
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully!";
             return RedirectToAction("Index");
         }
     }
--- Models/Category.cs
+++ Models/Category.cs
@@ -7,10 +7,15 @@ namespace BulkyWeb.Models
     {
         [Key] 
         public int Id { get; set; }
-        [Required]
+
         [DisplayName("Category Name")]
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
         [DisplayName("Display Order")]
+        [Required]
+        [Range(1,100, ErrorMessage = "The field Display Order field must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
